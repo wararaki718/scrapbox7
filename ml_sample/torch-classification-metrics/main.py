92a7844aac4b2be7a90a93c26d094b46d652f61c
wararaki <ky7.ott.w@gmail.com>
2024-07-28 18:09:59 +0900

    update

--- ml_sample/torch-classification-metrics/main.py
+++ ml_sample/torch-classification-metrics/main.py
@@ -11,10 +11,12 @@ from utils import try_gpu
 
 
 def main() -> None:
+    # load data
     iris = load_iris()
     X = torch.Tensor(iris.data)
     y = torch.Tensor(iris.target).long()
 
+    # setup dataset & dataloader
     print("dataset:")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
     X_valid, X_test, y_valid, y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
@@ -32,6 +34,7 @@ def main() -> None:
     model = NNModel(X.shape[1], len(set(y)))
     try_gpu(model)
 
+    # train
     print("train:")
     trainer = Trainer(n_epochs=30)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
