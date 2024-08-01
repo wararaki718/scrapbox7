cfcc49ca46577ffd72c58aeef0fc7ffa2adf9d52
wararaki <ky7.ott.w@gmail.com>
2024-08-01 20:44:03 +0900

    update

--- ml_sample/torch-classification-metrics/metrics/auc.py
+++ ml_sample/torch-classification-metrics/metrics/auc.py
@@ -1,24 +1,23 @@
+from typing import Optional
+
 import torch
 
 
-def roc_curve(y_true: torch.Tensor, y_score: torch.Tensor) -> float:
+def roc_curve(y_true: torch.Tensor, y_score: torch.Tensor, pos_label: Optional[int]=None) -> float:
+    y_true = (y_true == pos_label).long()
+
     indices = torch.argsort(y_score, descending=True)
     y_score = y_score[indices]
     y_true = y_true[indices]
-    # print(y_score)
-    # print(y_true)
-    # print()
+
     distinct_value_indices = torch.where(torch.diff(y_score))[0]
-    # print(distinct_value_indices)
     threshold_idxs = torch.concat([distinct_value_indices, torch.Tensor([len(y_true) - 1])]).long()
-    # print(threshold_idxs)
 
     tps = torch.cumsum(y_true, dim=0)[threshold_idxs]
-    # print(tps)
-    fps = 1 + threshold_idxs - tps
 
+    fps = 1 + threshold_idxs - tps
     thresholds = y_score[threshold_idxs]
-    
+
     tps = torch.concat([torch.Tensor([0]), tps])
     fps = torch.concat([torch.Tensor([0]), fps])
     thresholds = torch.concat([torch.Tensor([float("inf")]), thresholds])
@@ -36,7 +35,9 @@ def auc_score(tps: torch.Tensor, fps: torch.Tensor) -> float:
 if __name__ == "__main__":
     a = torch.Tensor([1, 1, 2, 2])
     b = torch.Tensor([0.1, 0.4, 0.35, 0.8])
-    tps, fps, _ = roc_curve(a, b)
+    tps, fps, thresholds = roc_curve(a, b, pos_label=2)
     print(tps)
     print(fps)
+    print(thresholds)
     print(auc_score(tps, fps))
+    print()
