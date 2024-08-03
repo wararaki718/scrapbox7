484cc987e17e94249a39621304ca444b073bc5ab
wararaki <ky7.ott.w@gmail.com>
2024-08-03 18:13:40 +0900

    update

--- nlp_sample/encoder-decoder-model/main.py
+++ nlp_sample/encoder-decoder-model/main.py
@@ -1,49 +1,45 @@
-from transformers import BertConfig, BertTokenizer, EncoderDecoderConfig, EncoderDecoderModel
+from transformers import BertConfig, BertTokenizer, EncoderDecoderConfig, EncoderDecoderModel, BertTokenizerFast
 
 
 def main() -> None:
     model_name = "google-bert/bert-base-uncased"
+    tokenizer = BertTokenizerFast.from_pretrained(model_name)
+    model = EncoderDecoderModel.from_encoder_decoder_pretrained(model_name, model_name)
+    print("model loaded!")
 
-    encoder_config = BertConfig()
-    decoder_config = BertConfig()
+    model.save_pretrained("model/bert2bert")
+    print(model.config)
 
-    config = EncoderDecoderConfig.from_encoder_decoder_configs(encoder_config, decoder_config)
-    model = EncoderDecoderModel(config)
-
-    encoder_config = model.config.encoder
-    decoder_config = model.config.decoder
-
-    decoder_config.is_decoder = True
-    decoder_config.add_cross_attention = True
-
-    model_path = "model/bert2bert"
-    model.save_pretrained(model_path)
-    print("model saved!")
-
-    encoder_decoder_config = EncoderDecoderConfig.from_pretrained(model_path)
-    # model = EncoderDecoderModel.from_encoder_decoder_pretrained(model_path, config=encoder_decoder_config)
-    model = EncoderDecoderModel.from_pretrained(model_path)
-    
-    tokenizer = BertTokenizer.from_pretrained(model_name)
+    # config
     model.config.decoder_start_token_id = tokenizer.cls_token_id
     model.config.eos_token_id = tokenizer.sep_token_id
     model.config.pad_token_id = tokenizer.pad_token_id
     model.config.vocab_size = model.config.encoder.vocab_size
+    model.config.max_length = 142
+    model.config.min_length = 56
+    model.config.no_repeat_ngram_size = 3
+    model.config.early_stopping = True
+    model.config.length_penalty = 2.0
+    model.config.num_beams = 4
 
+    # generate
     text = (
         "PG&E stated it scheduled the blackouts in response to forecasts for high winds "
         "amid dry conditions. The aim is to reduce the risk of wildfires. Nearly 800 thousand customers were "
         "scheduled to be affected by the shutoffs which were expected to last through at least midday tomorrow."
     )
     output = tokenizer(text, return_tensors="pt")
-    # print(output)
     input_ids = output.input_ids
+    print(input_ids)
+    print()
 
-    generated_ids = model.generate(input_ids)
-    print(generated_ids)
-    generated_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    print(generated_text)
+    result = model.generate(input_ids)
+    print(result)
+    print()
 
+    tokens = tokenizer.decode(result[0], skip_special_tokens=True)
+    print(tokens)
+    
     print("DONE")
 
 
