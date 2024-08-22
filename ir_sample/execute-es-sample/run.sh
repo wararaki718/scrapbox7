fedcd0964436ec78b48aa6ba36cd5ad5833dbd18
wararaki <ky7.ott.w@gmail.com>
2024-08-22 23:44:50 +0900

    update

--- ir_sample/execute-es-sample/run.sh
+++ ir_sample/execute-es-sample/run.sh
@@ -9,7 +9,7 @@ curl $APP_URL/articles | jq
 suffix=`date +"%S"`
 username=`echo "sample${suffix}"`
 
-# create user
+# user
 response=`curl -XPOST -H "Content-Type: application/json" $APP_URL/users -d "{\"user\":{\"username\":\"${username}\",\"email\":\"${username}@mail.com\",\"password\":\"pswd\"}}"`
 token=`echo $response | jq -r '.user.token'`
 echo $username
@@ -22,5 +22,16 @@ echo $token
 
 response=`curl -XGET -H "Authorization:Basic ${token}" $APP_URL/user`
 echo $response
+
+# article
+response=`curl -XPOST -H "Content-Type: application/json" -H "Authorization:Basic ${token}" $APP_URL/articles -d "{\"article\":{\"title\": \"${username}\",\"description\":\"${username}\",\"body\":\"${username}\"}}"`
+echo $response
+
+response=`curl -XGET -H "Authorization:Basic ${token}" $APP_URL/articles`
+echo $response
+
+response=`curl -XGET -H "Authorization:Basic ${token}" $APP_URL/articles/feed`
+echo $response
+
 
 echo "DONE"
