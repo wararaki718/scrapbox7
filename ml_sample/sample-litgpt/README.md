5748458538c07b93a415d7d94b362988ba9ca8e0
wararaki <ky7.ott.w@gmail.com>
2025-01-19 22:51:39 +0900

    update

--- ml_sample/sample-litgpt/README.md
+++ ml_sample/sample-litgpt/README.md
@@ -8,8 +8,17 @@ https://github.com/Lightning-AI/litgpt
 pip install 'litgpt[all]'
 ```
 
+model list
+
+```shell
+litgpt download list
+```
+
 ## run
 
 ```shell
+HF_TOKEN=$HUGGINGFACE_TOKEN
 python main.py
 ```
+
+use 16GB memory
--- ml_sample/sample-litgpt/main.py
+++ ml_sample/sample-litgpt/main.py
@@ -2,9 +2,11 @@ from litgpt import LLM
 
 
 def main() -> None:
-    llm = LLM.load("EleutherAI/pythia-14m")
+    llm = LLM.load("microsoft/Phi-3.5-mini-instruct")
     text = llm.generate("Fix the spelling: Every fall, the familly goes to the mountains.")
+    print("output:")
     print(text)
+    print()
 
     print("DONE")
 
