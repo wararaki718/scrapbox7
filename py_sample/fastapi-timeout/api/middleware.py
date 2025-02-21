459467f96aee832d5ec880abad3a95a5fe5b3e47
wararaki <ky7.ott.w@gmail.com>
2025-02-21 21:42:44 +0900

    update

--- py_sample/fastapi-timeout/api/middleware.py
+++ py_sample/fastapi-timeout/api/middleware.py
@@ -1,16 +1,16 @@
 import asyncio
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
-from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi.responses import JSONResponse, Response
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 
 
 class TimeoutMiddleware(BaseHTTPMiddleware):
     def __init__(self, app: FastAPI, timeout: int = 10):
         super().__init__(app)
-        self.timeout = timeout
+        self._timeout = timeout
 
-    async def dispatch(self, request: Request, call_next):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> JSONResponse | Response:
         try:
-            return await asyncio.wait_for(call_next(request), timeout=self.timeout)
+            return await asyncio.wait_for(call_next(request), timeout=self._timeout)
         except asyncio.TimeoutError:
             return JSONResponse({"detail": "Request timeout"}, status_code=504)
